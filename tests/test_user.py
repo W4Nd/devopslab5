3fad055d91b4748d823d2dd667463fdde38b7ac9
W4Nd <strugovmaks435@gmail.com>
2025-04-06 23:18:12 +0300

    changes3

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,5 +1,8 @@
 from fastapi.testclient import TestClient
 
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 from src.main import app
 
 client = TestClient(app)
